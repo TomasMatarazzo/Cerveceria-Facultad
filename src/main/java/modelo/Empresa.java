012565826938a16b0a972aa257e26bd4978d1548
ignaciosuarezquilis <ignaciogustavosuarez1234@gmail.com>
2022-11-07 10:06:24 -0300

    agreego clases

---
+++ src/main/java/modelo/Empresa.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class Empresa {
+    private String nombre;
+    private Mozo mozos;
+    private Mesa mesas;
+    private Producto productos;
+    private Operario operarios;
+        
+}
