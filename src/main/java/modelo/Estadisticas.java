eb7661cf3d86d96d8c35e808b6c21233e2d40682
TomasMatarazzo <tomasm2008@hotmail.es>
2022-11-07 18:42:36 -0300

    hola

---
+++ src/main/java/modelo/Estadisticas.java
@@ -0,0 +1,36 @@
+package modelo;
+
+import java.util.TreeSet;
+
+public class Estadisticas {
+
+    public static Mozo mayorVolumenVenta(TreeSet<Mozo> mozos){
+        double max = -10;
+        Mozo mozoMax = null;
+        for (Mozo mozo:mozos){
+            if (mozo.getVentas() > max){
+                max = mozo.getVentas();
+                mozoMax = mozo;
+            }
+        }
+
+        return mozoMax;
+    }
+
+    public static Mozo menorVolumenMesa(TreeSet<Mozo> mozos){
+        double min = 10000000;
+        Mozo mozoMax = null;
+        for (Mozo mozo:mozos){
+            if (mozo.getVentas() < min){
+                min = mozo.getVentas();
+                mozoMax = mozo;
+            }
+        }
+
+        return mozoMax;
+    }
+
+    public static double promedioPorMesa(){
+        System.out.printf("completar");
+    }
+}
