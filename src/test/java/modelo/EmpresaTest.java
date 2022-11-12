90ba591e11605b0a6dd2b12ba9cffa6be6a0e24f
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2022-11-12 15:16:22 -0300

    ahora si

---
+++ src/test/java/modelo/EmpresaTest.java
@@ -0,0 +1,33 @@
+package modelo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class EmpresaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void signup() {
+    }
+
+    @Test
+    void modificarPassword() {
+    }
+
+    @Test
+    void altaProducto() {
+    }
+}
