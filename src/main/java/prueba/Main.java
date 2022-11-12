88adb6ed5ee205dc9b08087178090c521b9791d2
ignaciosuarezquilis <ignaciogustavosuarez1234@gmail.com>
2022-11-12 19:35:04 -0300

    mas

--- src/main/java/prueba/Main.java
+++ src/main/java/prueba/Main.java
@@ -20,28 +20,26 @@ public class Main {
         Empresa cerveceria=null;
 
         try {
-            IPersistencia persistencia=new PersistenciaBIN();
-
             try {
+                IPersistencia persistencia=new PersistenciaBIN();
                 persistencia.abrirInput("Cerveceria.bin");
-                System.out.println("Archivo abierto");
                 cerveceria=(Empresa) persistencia.leer();
                 persistencia.cerrarInput();
             }catch (IOException e){
                 System.out.println(e.getMessage());
             }
 
-            TreeSet<Mozo> mozos = cerveceria.getMozos();
+            /*TreeSet<Mozo> mozos = cerveceria.getMozos();
             TreeSet<Producto> productos = cerveceria.getProductos();
             TreeSet<Operario> operarios = cerveceria.getOperarios();
-            TreeSet<Mesa> mesas=cerveceria.getMesas();
+            TreeSet<Mesa> mesas=cerveceria.getMesas();*/
+            TreeSet<Operario> operarios=new TreeSet<>();
+            operarios.add(new Operario("ADMIN","ADMIN","ADMIN1234",true));
 
-            operarios.add(new Operario("SUAREZ","IGNACIO","123",true));
-
-            System.out.println(operarios.toString());
+            //System.out.println(operarios.toString());
 
             VistaLogin vista = new VistaLogin();
-            Empresa empresa = new Empresa("CerveceriaProgra",mozos,mesas,productos,operarios);
+            Empresa empresa = new Empresa("CerveceriaProgra",null,null,null,operarios);
 
             FormPassword form = new FormPassword();
             ControladorLogin controlador = new ControladorLogin(vista,empresa,form);
